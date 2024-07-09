a5b47e9d40f0f86dc60d885fa5e72b841aad2702
ShreerajM <svmhatre58@gmail.com>
2024-07-10 01:34:32 +0530

    feat: Adds middleware and api_router to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,29 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = ["http://localhost:3000", "localhost:3000"]
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+api_router = APIRouter(
+    prefix="/api",
+    responses={
+        200: {"description": "Success"},
+        201: {"description": "Created"},
+        204: {"description": "No Content"},
+        400: {"description": "Bad Request"},
+        401: {"description": "Unauthorized"},
+        403: {"description": "Forbidden"},
+        404: {"description": "Not found"},
+        500: {"description": "Internal Server Error"},
+    }
+)
+
+app.include_router(api_router)
